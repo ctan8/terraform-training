17986be7692226be674a47f4c0723260aa74c0ac
USER <calvyn.tan@endava.com>
2024-07-22 15:54:30 +0100

    Updates

---
+++ day2-terraform-service-account/bigquery/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  default     = "dna-tfm-trn-2475"
+}
+
+variable "region" {
+  description = "The GCP region"
+  default     = "europe-west2"
+}
+
+variable "bigquery_dataset_id" {
+  description = "Name of the BigQuery dataset"
+  default     = "calvyn_terraform_dataset"
+}
+
+variable "bigquery_table_id" {
+  description = "Name of the BigQuery table"
+  default     = "calvyn_terraform_table"
+}
